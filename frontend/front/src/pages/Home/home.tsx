abd81634dcf328e96a9290bbe21a57d3c07f047b
AimanTegisbsyeva <aimantegisbayeva@gmail.com>
2026-07-12 21:32:42 +0500

    front2

---
+++ frontend/front/src/pages/Home/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Book  from '../../components/Book/book.tsx';
+
+export default function Home() {
+  return (
+    <div>
+      
+      <Book />
+      
+    </div>
+  );
+}
